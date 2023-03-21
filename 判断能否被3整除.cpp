5fc701316aec8495100110a2e1e26e5da0ba3124
LeYushu <128287513+LeYushu@users.noreply.github.com>
2023-03-21 16:05:08 +0800

    Add files via upload

---
+++ 判断能否被3整除.cpp
@@ -0,0 +1,18 @@
+﻿// 键盘输入有一个数判断能否被3整除
+#include <stdio.h>
+int main(void)
+{
+	int x,y;
+	printf("请输入一个数：");
+	scanf_s("%d",&x);
+	y = x % 3;
+	if (y == 0)
+	{
+		printf("%d可以被3整除", x);
+	}
+	else
+	{
+		printf("%d不可以被3整除",x);
+	}
+	return 0;
+}
