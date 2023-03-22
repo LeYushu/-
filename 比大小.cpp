77ac60fc3eeeceac52adb5d4dcc4373549d1dc36
LeYushu <128287513+LeYushu@users.noreply.github.com>
2023-03-22 13:09:34 +0800

    Add files via upload

---
+++ 比大小.cpp
@@ -0,0 +1,20 @@
+﻿//三个整数比较大小，输出最小值。
+#include <stdio.h>
+int main(void)
+{
+	int x, y, z;
+	printf("请输入三个整数：");
+	scanf_s("%d%d%d", &x,& y,& z);
+	if (x > y)
+		if (y > z)
+			printf("最小的数是：%d", z);
+		else
+			printf("最小的数是：%d", y);
+	else if (y > x)
+		if (x > z)
+			printf("最小的数是：%d", z);
+		else
+			printf("最小的数是：%d", x);
+
+	return 0;
+}
